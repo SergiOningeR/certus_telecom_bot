ba77fe98d520acf1acb1785649d840b3f9dd9033
SergiOningeR <gsa@certus-telecom.ru>
2025-05-18 02:45:31 +0600

    Update main.py

--- admin_panel/main.py
+++ admin_panel/main.py
@@ -1,6 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException, UploadFile, File
-from fastapi.responses import FileResponse
-from pathlib import Path
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import APIKeyHeader
 from sqlalchemy.orm import Session
 from database.models import Task
@@ -11,16 +9,9 @@ app = FastAPI(title="Certus Admin Panel")
 api_key_header = APIKeyHeader(name="X-API-Key")
 
 @app.get("/tasks/")
-async def get_task_image(task_id: int):
-    task = db.query(Task).filter(Task.id == task_id).first()
-    if not task.image_path:
-        raise HTTPException(404)
-    return FileResponse(Path(config.IMAGE_STORAGE) / task.image_path)
-
 async def get_tasks(api_key: str = Depends(api_key_header)):
     if api_key != config.ADMIN_API_KEY:
         raise HTTPException(status_code=403)
-    
     db = SessionLocal()
     try:
         tasks = db.query(Task).limit(100).all()
