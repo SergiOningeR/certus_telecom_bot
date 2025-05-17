09a7987b95a4082cc88c7b76be40c91980192f3c
SergiOningeR <gsa@certus-telecom.ru>
2025-05-18 01:04:41 +0600

    Update main.py

--- main.py
+++ main.py
@@ -52,3 +52,31 @@ async def main():
 if __name__ == '__main__':
     import asyncio
     asyncio.run(main())
+
+async def on_startup(dp):
+    if config.USE_WEBHOOK:
+        await dp.bot.set_webhook(
+            url=config.WEBHOOK_URL,
+            certificate=open(config.WEBHOOK_SSL_CERT, 'rb') if config.WEBHOOK_SSL_CERT else None
+        )
+
+async def on_shutdown(dp):
+    if config.USE_WEBHOOK:
+        await dp.bot.delete_webhook()
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+if __name__ == '__main__':
+    if config.USE_WEBHOOK:
+        from aiogram import executor
+        executor.start_webhook(
+            dispatcher=dp,
+            webhook_path=config.WEBHOOK_PATH,
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+            host=config.WEBAPP_HOST,
+            port=config.WEBAPP_PORT,
+        )
+    else:
+        import asyncio
+        asyncio.run(main())
