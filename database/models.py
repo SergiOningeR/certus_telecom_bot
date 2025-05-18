23194eef4fc0e881a9bc859044928934e594cafd
SergiOningeR <gsa@certus-telecom.ru>
2025-05-18 10:50:14 +0600

    Update models.py

--- database/models.py
+++ database/models.py
@@ -7,7 +7,7 @@ Base = declarative_base()
 
 class Task(Base):
     __tablename__ = 'tasks'
-    
+
     id = Column(Integer, primary_key=True)
     title = Column(String(100), nullable=False)
     description = Column(Text, nullable=False)
@@ -18,4 +18,6 @@ class Task(Base):
     status = Column(Enum(TaskStatus), default=TaskStatus.NEW)
     created_at = Column(DateTime, default=datetime.utcnow)
     telegram_user_id = Column(Integer, nullable=False)
+    telegram_username = Column(String(50))
     admin_comment = Column(Text)
+    completion_report = Column(Text)
